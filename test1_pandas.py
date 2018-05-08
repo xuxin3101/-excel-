4debe2cca0152eb98520fc01c2e6109da6f1361c
xuxin <310199939@qq.com>
2018-05-08 09:55:09 +0800

    pandas方法核心算法添加注释说明

--- test1_pandas.py
+++ test1_pandas.py
@@ -21,12 +21,13 @@ class data(object) :
         self.total_budget=0
 df=pd.read_excel('资料.xlsx',sheetname=3)
 first=[]
-for row in df.iterrows() :
-    flag=False
-    for r in first:
+########核心算法开始###########
+for row in df.iterrows() :#按行读取读出的数据
+    flag=False #设置标志位，检测当前店铺名是否已经存在到first列表
+    for r in first:#遍历first。检测当前店铺名是否已经存在到first列表
         if r.cd==row[1][1] :
-            flag = True
-            if row[1][7] == 1 or row[1][7] == 24 or row[1][7] == 31:
+            flag = True#存在置true，进行下面操作
+            if row[1][7] == 1 or row[1][7] == 24 or row[1][7] == 31:#三种店铺各种值相加
                 r.total_budget = r.total_budget + row[1][5]
                 r.performance = r.performance + row[1][3]
                 r.laseyear_performance = r.laseyear_performance + row[1][4]
@@ -42,7 +43,7 @@ for row in df.iterrows() :
                 else:
                     r.laseyearrate = 100
                     r.strlaseyearrate = '-'
-    if flag is False :
+    if flag is False :#说明first列表里没有当前店铺，新建一个data对象保存当前店铺数据
         item=data()
         item.yearweek=row[1][0]
         item.cd=row[1][1]
@@ -53,11 +54,13 @@ for row in df.iterrows() :
         item.laseyear_performance = row[1][4]
         if item.total_budget != 0 and item.area != 'nan':
             first.append(item)
-first.sort(key=lambda x: (x.laseyearrate, x.rate), reverse=True)
+first.sort(key=lambda x: (x.laseyearrate, x.rate), reverse=True)#规则排序
+######核心算法结束########
 for r in first :
         print (str(r.cd)+'   '+r.storyname+'    '+r.area+'  '+str(r.total_budget)+'    '+str(r.performance)+'  '+str(r.laseyear_performance)+' '+r.strrate+'   '+r.strlaseyearrate)
-df_answer=pd.read_excel('答案.xlsx',sheetname=0)
+
 writer = pd.ExcelWriter('output.xlsx')
+df_answer=pd.read_excel('答案.xlsx')
 df1=pd.DataFrame(data={'CD':[r.cd],'店名':[r.storyname]})
 df1.to_excel(writer)
 cdlist=[]
@@ -69,6 +72,7 @@ laseyear_performancelist=[]
 strratelist=[]
 strlaseyearratelist=[]
 emptylist=[]
+count=3
 for r in first :
     cdlist.append(r.cd)
     storynamelist.append(r.storyname)
@@ -79,8 +83,9 @@ for r in first :
     strratelist.append(r.strrate)
     strlaseyearratelist.append(r.strlaseyearrate)
     emptylist.append('')
+    count=count+1
 
 df1 = pd.DataFrame({'順位':emptylist,'CD': cdlist, '店名': storynamelist,'エリア名':arealist,'予算':total_budgetlist,'昨年実績':laseyear_performancelist,'実績':performancelist,'予算比':strratelist,'昨対比':strlaseyearratelist})
-
+df_answer.to_excel(writer,index=False)
 df1.to_excel(writer,index=False)
 writer.save()
